bc1e7b3941085e583641c96078abed9179e08654
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-03-20 16:02:42 +0100

    updated movie interface

--- src/adminsite/MoviesData.ts
+++ src/adminsite/MoviesData.ts
@@ -1,31 +1,20 @@
 export interface Movie {
   id?: number;
   title: string;
+  description: string;
+  actors: string;
   duration: number;
+  genre: string;
   is3D: boolean;
-  isActive: boolean;
+  active: boolean;
 }
 
-export const MoviesList: Movie[] = [
-  { id: 1, title: "Max Power", duration: 120, is3D: false, isActive: false },
-  { id: 2, title: "O'boy", duration: 170, is3D: true, isActive: true },
-  { id: 3, title: "Title", duration: 150, is3D: true, isActive: false },
-  {
-    id: 4,
-    title: "The Little Killermachine",
-    duration: 180,
-    is3D: false,
-    isActive: true,
-  },
-  {
-    id: 5,
-    title: "Groundhog day",
-    duration: 30,
-    is3D: false,
-    isActive: false,
-  },
-
-  { id: 6, title: "Dry Paint", duration: 20, is3D: false, isActive: false },
-  { id: 7, title: "Lame movie", duration: 70, is3D: true, isActive: true },
-  { id: 8, title: "The Stabler", duration: 50, is3D: true, isActive: false },
-];
+// BACKEND...
+// private int id;
+//   private String title;
+//   private String description;
+//   private String actors;
+//   private int duration;
+//   private String genre;
+//   private boolean is3D;
+//   private boolean isActive;
