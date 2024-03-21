0dd1a6120a46b48bfb26372de0cf809bca04e145
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-03-21 15:44:16 +0100

    remove dead links

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
 
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Layout.tsx";
 import AdminMovies from "./adminsite/admin-movies.tsx";
-import CustomerMovies from "./customersite/customer-movies.tsx";
-// import CustomerView from "./customersite/CustomerView.tsx";
-// import AdminView from "./adminsite/AdminView.tsx";
-// import Navigation from "./Navigation.tsx";
 
 const App: React.FC = () => {
   return (
@@ -22,7 +16,6 @@ const App: React.FC = () => {
           {/* <Route index element={<Recipes />} /> */}
           <Route path=":id" />
         </Route>
-        {/*  vi skal have noget "admin" authorization på vores knap - så vi bliver sendt til login siden!*/}
       </Routes>
     </Layout>
   );
