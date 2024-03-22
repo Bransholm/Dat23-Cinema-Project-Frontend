9cfa934f4c5dc832a2acf11a2e8943f5a1a12a08
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-03-22 15:11:27 +0100

    post - and put routes, plus movie interfaces moved to movieAPI test

--- src/services/movieAPItest.ts
+++ src/services/movieAPItest.ts
@@ -1,27 +1,61 @@
 import { handleHttpErrors } from "./fetchUtils";
 import { API_URL } from "../settings";
+// import { Movie } from "../adminsite/MoviesData";
 
 const MOVIE_URL = API_URL + "/movies";
 
 interface movie {
-  id: number ;
+  id?: number;
   title: string;
   description: string;
   actors: string;
   duration: number;
   genre: string;
-  is3D: boolean;
-  isActive: boolean;
+  threeD: boolean;
+  active: boolean;
+}
+
+function MoviePutRoute(updatedMovie: movie): Promise<movie> {
+  console.log("put-route-data", updatedMovie);
+  return fetch(`${MOVIE_URL}/${updatedMovie.id}`, {
+    method: "PUT",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(updatedMovie),
+  }).then((response) => {
+    console.log("put-route-response", response); // Log the response here
+    if (!response.ok) {
+      throw new Error("An error occured while updating the movie");
+    }
+    return response.json() as Promise<movie>;
+  });
+}
+
+function MoviePostRoute(createdMovie: movie): Promise<movie> {
+  console.log("post-route-data", createdMovie);
+  return fetch(`${MOVIE_URL}`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(createdMovie),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("An error occured while updating the movie");
+    }
+    return response.json() as Promise<movie>;
+  });
 }
 
 async function getMovies(): Promise<Array<movie>> {
-    console.log("fetchMovies");
-    
+  console.log("fetchMovies");
+
   return fetch(MOVIE_URL).then(handleHttpErrors);
 }
 
 // test that i get all the movies
-getMovies().then(res => console.log(res))
+getMovies().then((res) => console.log(res));
 
 async function getMovie(id: number): Promise<movie> {
   return fetch(MOVIE_URL + "/" + id).then(handleHttpErrors);
@@ -29,4 +63,4 @@ async function getMovie(id: number): Promise<movie> {
 
 export type { movie };
 
-export { getMovies, getMovie };
+export { getMovies, getMovie, MoviePutRoute, MoviePostRoute };
