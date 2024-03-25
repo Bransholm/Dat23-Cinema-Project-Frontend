62fd30c9bd86d503ff0557eab31d884c414a0e81
SirShagalot <124779092+luma0001@users.noreply.github.com>
2024-03-25 19:24:10 +0100

    authProvider implemented

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from "./App.tsx";
 
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
+import AuthProvider from "./security/AuthProvider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </React.StrictMode>
 );
